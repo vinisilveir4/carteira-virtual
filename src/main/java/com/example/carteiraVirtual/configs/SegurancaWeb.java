f9ec4bfcf8ce60f4c17387f92f1ac829c90aba7d
vinisilveir4 <viniciussilverk@gmail.com>
2024-06-22 02:22:28 -0300

    habilita acesso

---
+++ src/main/java/com/example/carteiraVirtual/configs/SegurancaWeb.java
@@ -0,0 +1,23 @@
+package com.example.carteiraVirtual.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SegurancaWeb {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
